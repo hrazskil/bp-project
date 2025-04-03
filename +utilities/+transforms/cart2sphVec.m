1ea1444ddcb206fa60f582ed0ada954e6ee31e9a
jelinel1 <jelinel1@fel.cvut.cz>
2025-04-03 09:52:02 +0200

    correct evaluation of spherical vectors on z-axis

--- +utilities/+transforms/cart2sphVec.m
+++ +utilities/+transforms/cart2sphVec.m
@@ -10,14 +10,18 @@ function [F_r, F_theta, F_phi] = cart2sphVec(x, y, z, F_x, F_y, F_z)
   
     % Repeated calculations for radius and xy-plane distance
     r = sqrt(x.^2 + y.^2 + z.^2);   % Radial distance from the origin
-    xy = sqrt(x.^2 + y.^2);         % Distance in the xy-plane
+    rho = sqrt(x.^2 + y.^2);         % Distance in the xy-plane
+    phi = angle(x + 1i*y); % spherical angle phi
 
     % Calculate the radial component of the vector
     F_r = (x.*F_x + y.*F_y + z.*F_z) ./ r;
     
     % Calculate the Polar component of the vector
-    F_theta = (F_x.*z.*x + F_y.*z.*y - F_z.*xy.^2) ./ (r.*xy);
-    
+    % F_theta = (F_x.*z.*x + F_y.*z.*y - F_z.*rho.^2) ./ (r.*rho);
+    F_theta = (F_x.*z.*cos(phi) + F_y.*z.*sin(phi) - F_z.*rho)./r;
+
     % Calculate the azimuthal component of the vector
-    F_phi = (-F_x.*y + F_y.*x) ./ xy;
+    % F_phi = (-F_x.*y + F_y.*x) ./ rho;
+    F_phi = (-F_x.*sin(phi) + F_y.*cos(phi)) ;
+
 end
